9f169b93544c68f298fe0e5167d359979341c0f5
polis89 <89polis@gmail.com>
2018-01-04 21:47:26 +0100

    Added Vacancies

--- app/src/vues/vacancy.vue
+++ app/src/vues/vacancy.vue
@@ -2,9 +2,58 @@
   <transition 
     name="inleft">
     <div class="vacancy-page">
-      <div class="wrapper-job">
-        ВАКАНСИИ
+      <div class="wrapper-vacancy">
+        <div class="left" v-html="pageData.mainHtml">
+        </div>
+        <div class="right">
+          <div class="img-cont">
+            <img :src="pageData.mainImg" alt="img">
+          </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="steps">
+          <div class="step" v-for="(step, index) in pageData.steps">
+            <div class="num">{{ index+1 }}</div>
+            <p v-html="step">
+            </p>
+          </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="vacancys">
+          <div class="vacancy" v-for="(vacancy, index) in pageData.vacancys">
+            <div class="name">Вакансия <strong>{{ vacancy.name }}</strong></div>
+            <div class="text"><span v-html="vacancy.desc"></span>
+            <span class="open" v-on:click="vacancy.isOpen=false" v-if="vacancy.isOpen">Закрыть</span><span v-on:click="vacancy.isOpen=true" class="open" v-else>Читать далее</span></div>
+            <div class="drop-cont" v-if="vacancy.isOpen">
+              <div class="column">
+                <div class="col-name">Чек-лист наших "хотений":</div>
+                <ul>
+                  <li v-for="wish in vacancy.wishes" v-html="wish">
+                  </li>
+                </ul>
+              </div>
+              <div class="column">
+                <div class="col-name">Чек-лист твоих задач:</div>
+                <ul>
+                  <li v-for="task in vacancy.tasks">
+                    {{ task }}
+                  </li>
+                </ul>
+                <div class="col-name">Что ты получишь?</div>
+                <ul>
+                  <li v-for="get in vacancy.gets">
+                    {{ get }}
+                  </li>
+                </ul>
+                <div class="col-name">Нам не хватает тебя. Отправить заявочку.</div>
+                <a href="mailto:job@gosocial.md">job@gosocial.md</a>
+              </div>
+              <div class="clearfix"></div>
+            </div>
+          </div>
+        </div>
       </div>
+      <app-footer></app-footer>
     </div>
   </transition>
 </template>
@@ -23,11 +72,8 @@ export default {
   data () {
     return {
       // pageShowed: false,
-      job: {},
+      pageData: { }
     }
-  },
-  beforeRouteEnter (to, from, next) {
-    next();
   },
   computed: {
   },
@@ -35,7 +81,68 @@ export default {
     btnClick: function (event) {
       this.$emit('menuClick');
     },
-  }
+  },
+  beforeRouteEnter (to, from, next) {
+    var pageData = {};
+    var pageID = 65;
+    console.log("show Vacancyes");
+    axios.get(rest.AJAX_URL + '/wp/v2/pages/' + pageID + '?_embed')
+      .then(function (response) {
+        var responseData = response.data;
+        // console.log(responseData);
+        pageData.mainHtml = responseData.acf["котент"];
+        pageData.mainImg = responseData.acf["изображение"];
+        pageData.steps = [];
+        if(responseData.acf["шаги"]){
+          for (var i = 0; i < responseData.acf["шаги"].length; i++){
+            pageData.steps.push(responseData.acf["шаги"][i]["шаг"]);
+          }
+        }
+        axios.get(rest.AJAX_URL + '/wp/v2/vacancies?_embed')
+          .then(function (response){
+            var responseData = response.data;
+            console.log(responseData);
+            pageData.vacancys = [];
+            if(responseData){
+              for (var i = 0; i < responseData.length; i++){
+                var vac = {
+                  isOpen: false,
+                  name: responseData[i].title.rendered,
+                  desc: responseData[i].acf.desc,
+                  wishes: [],
+                  tasks: [],
+                  gets: [],
+                };
+                if(responseData[i].acf["wishes"]){
+                  for (var j = 0; j < responseData[i].acf["wishes"].length; j++){
+                    vac.wishes.push(responseData[i].acf["wishes"][j]["wish"]);
+                  }
+                }
+                if(responseData[i].acf["tasks"]){
+                  for (var j = 0; j < responseData[i].acf["tasks"].length; j++){
+                    vac.tasks.push(responseData[i].acf["tasks"][j]["task"]);
+                  }
+                }
+                if(responseData[i].acf["gets"]){
+                  for (var j = 0; j < responseData[i].acf["gets"].length; j++){
+                    vac.gets.push(responseData[i].acf["gets"][j]["get"]);
+                  }
+                }
+                pageData.vacancys.push(vac);
+              }
+            }
+            next(vm => vm.pageData = pageData);
+
+          })
+          .catch(function (error) {
+            console.log(error.message);
+        });
+      })
+      .catch(function (error) {
+        console.log(error.message);
+    });
+
+  },
 }
 </script>
 
